f0f2861b0165201f0649aadcc3aee9a940f13bfa
mgflok1 <134943906+mgflok1@users.noreply.github.com>
2024-04-12 23:47:44 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,14 @@
+#include "Automata.h"
 
+int main() {
+    Automata vendingMachine;
+    vendingMachine.on();
+    vendingMachine.coin(10);
+    vendingMachine.coin(5);
+    vendingMachine.getMenu({"Coffee", "Tea"}, {10, 5});
+    vendingMachine.choice(0);
+    vendingMachine.check(0);
+    vendingMachine.finish();
+
+    return 0;
+}

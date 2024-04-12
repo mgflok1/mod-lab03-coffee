decd38786ea4b4d12943b89e523a0c13627c28b8
mgflok1 <134943906+mgflok1@users.noreply.github.com>
2024-04-12 23:47:13 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,1 +1,70 @@
+#include "Automata.h"
+#include <iostream>
+
+Automata::Automata() {
+    cash = 0;
+    state = OFF;
+}
+
+void Automata::on() {
+    state = WAIT;
+    std::cout << "Автомат в ожидании\n";
+}
+
+void Automata::off() {
+    state = OFF;
+    std::cout << "Автомат выключен\n";
+}
+
+void Automata::coin(int money) {
+    cash += money;
+    std::cout << "Добавлено " << money << " рублей. Текущий баланс " << cash << "\n";
+}
+
+void Automata::getMenu(const std::vector<std::string>& menu, const std::vector<int>& prices) {
+    this->menu = menu;
+    this->prices = prices;
+}
+
+STATES Automata::getState() {
+    return state;
+}
+
+void Automata::choice(int option) {
+    if (option >= 0 && option < menu.size()) {
+        state = CHECK;
+        std::cout << "Выбрано: " << menu[option] << ". Пожалуйста, подождите, пока пройдет проверка\n";
+    } else {
+        std::cout << "Invalid option.\n";
+    }
+}
+
+bool Automata::check(int option) {
+    if (cash >= prices[option]) {
+        state = COOK;
+        std::cout << "Денег достаточно. " << menu[option] << " готовится\n";
+        return true;
+    } else {
+        std::cout << "Не хватает денег, пополните баланк\n";
+        return false;
+    }
+}
+
+void Automata::cancel() {
+    cash = 0;
+    state = WAIT;
+    std::cout << "Транзация отменена.\n";
+}
+
+void Automata::cook() {
+    std::cout << "Напиток готов\n";
+    cash -= prices[0];
+    state = WAIT;
+}
+
+void Automata::finish() {
+    cash = 0;
+    state = WAIT;
+    std::cout << "Транзакция завершена\n";
+}
 

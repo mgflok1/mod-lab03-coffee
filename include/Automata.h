f3136bb2134dc891e574515300fc392edb5caf28
mgflok1 <134943906+mgflok1@users.noreply.github.com>
2024-04-12 23:48:58 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,1 +1,36 @@
 
+//
+// Created by Максим Глвдков on 09.04.2024.
+//
+
+#ifndef AUTOMATA_H
+#define AUTOMATA_H
+
+#include <string>
+#include <vector>
+
+enum STATES {OFF, WAIT, ACCEPT, CHECK, COOK};
+
+class Automata {
+private:
+    int cash;
+    std::vector<std::string> menu;
+    std::vector<int> prices;
+    STATES state;
+
+public:
+    Automata();
+
+    void on();
+    void off();
+    void coin(int money);
+    void getMenu(const std::vector<std::string>& menu, const std::vector<int>& prices);
+    STATES getState();
+    void choice(int option);
+    bool check(int option);
+    void cancel();
+    void cook();
+    void finish();
+};
+
+#endif //AUTOMATA_H
